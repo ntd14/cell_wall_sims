52c29d5b0925dcee861d70b3f804a575c3b9ffd2
ntd14 <ntd14@uclive.ac.nz>
2015-05-21 17:10:34 +1200

    start of skeleton

--- twoD_single_pore.c
+++ twoD_single_pore.c
@@ -3,6 +3,19 @@
  *
  *  Created on: 21/05/2015
  *      Author: nick
+ *
+ *  This is the main trunk file for the implementation of a single 2D cellulose fibril aggregate
+ *  pore filling with lignin.
  */
 
 
+#include <stdio.h>
+
+int main(void) /*this may change to take in arguments later*/
+{
+	printf("call to a function that will load ini file of problem and partical properties and return them");
+	printf("passing partical and problem properties to function that will build init state and return it");
+	printf("pass out init state to a function that will step loop it in time and return final state, potentually save each time step");
+	printf("return final timestep properties and farm out to post processing to calc strains etc");
+	return 0;
+}
