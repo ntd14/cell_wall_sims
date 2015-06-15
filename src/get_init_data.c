eb75e2652d254926fb2df48f84a486db5f710fb2
ntd14 <ntd14@uclive.ac.nz>
2015-06-15 17:18:19 +1200

    starting to get multiple FAs into model, 2D still

--- src/get_init_data.c
+++ src/get_init_data.c
@@ -21,16 +21,38 @@ void create_init_state()
 	tmp_sp[0] = FA_starting_points[ind2D(0,0,10,3)];
 	tmp_sp[1] = FA_starting_points[ind2D(0,1,10,3)];
 	tmp_sp[2] = FA_starting_points[ind2D(0,2,10,3)];
-	double* init_pos = make2Darray(2*max_build_steps, 5);
-	int len_FA = new_point_pos(init_pos, tmp_sp, FA1);
-	for(ii=0; ii < len_FA; ii++)
-	{
-		printf("%f, ",init_pos[ind2D(ii, 0, len_FA, 5)]);
-		printf("%f, ",init_pos[ind2D(ii, 1, len_FA, 5)]);
-		printf("%f, ",init_pos[ind2D(ii, 2, len_FA, 5)]);
-		printf("%f, ",init_pos[ind2D(ii, 3, len_FA, 5)]);
-		printf("%f, \n",init_pos[ind2D(ii, 4, len_FA, 5)]);
-	}
+	double* init_pos = make3Darray(2*max_build_steps, 5, 2);
 
+	int len_FA = new_point_pos((init_pos+ind3D(0,0,0, 2*max_build_steps, 5, 2)), tmp_sp, FA1);
+	len_FA = new_point_pos((init_pos+ind3D(0,0,1, 2*max_build_steps, 5, 2)), tmp_sp, FA1);
+/*	double* init_pos = make2Darray(2*max_build_steps, 5);
+	int len_FA = new_point_pos(init_pos, tmp_sp, FA1); */
+/*	for(ii=0; ii < len_FA; ii++)
+		{
+			printf("%f, ",  init_pos[ind3D(ii, 0, 0, len_FA, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(ii, 1, 0, len_FA, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(ii, 2, 0, len_FA, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(ii, 3, 0, len_FA, 5, 2)]);
+			printf("%f, \n",init_pos[ind3D(ii, 4, 0, len_FA, 5, 2)]);
+		}
+	printf("\n");
+	for(ii=0; ii < len_FA; ii++)
+		{
+			printf("%f, ",  init_pos[ind3D(ii, 0, 1, len_FA, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(ii, 1, 1, len_FA, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(ii, 2, 1, len_FA, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(ii, 3, 1, len_FA, 5, 2)]);
+			printf("%f, \n",init_pos[ind3D(ii, 4, 1, len_FA, 5, 2)]);
+		}*/
+	for(ii=0; ii < len_FA; ii++)
+		{
+			printf("%i, ",  ind3D(ii, 0, 0, len_FA, 5, 2));
+
+		}
+	printf("\n");
+	for(ii=0; ii < len_FA; ii++)
+		{
+			printf("%i, ",  ind3D(ii, 0, 1, len_FA, 5, 2));
+		}
 }
 
