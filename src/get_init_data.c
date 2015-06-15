1c7455fdc9c8e898cdb87ef3e917df8082eab2f4
Nick <ntd14@uclive.ac.nz>
2015-06-15 20:51:43 +1200

    multiple calls to make FAs. still no teminator for the array though

--- src/get_init_data.c
+++ src/get_init_data.c
@@ -21,8 +21,36 @@ void create_init_state()
 	tmp_sp[0] = FA_starting_points[ind2D(0,0,10,3)];
 	tmp_sp[1] = FA_starting_points[ind2D(0,1,10,3)];
 	tmp_sp[2] = FA_starting_points[ind2D(0,2,10,3)];
-	double* init_pos = make3Darray(2*max_build_steps, 5, 2);
+	double* init_pos = make3Darray(2*max_build_steps, 5, 5);
 	int len_FA = 2*max_build_steps;
+	int len_FA_old = 0;
+
+	for(ii = 0; ii < 5; ii++)
+	{
+		tmp_sp[0] = FA_starting_points[ind2D(ii,0,10,3)];
+		tmp_sp[1] = FA_starting_points[ind2D(ii,1,10,3)];
+		tmp_sp[2] = FA_starting_points[ind2D(ii,2,10,3)];
+		len_FA = new_point_pos((init_pos+ind3D(0,0,ii, 2*max_build_steps, 5, 5)), tmp_sp, len_FA, FA1);
+		if(len_FA > len_FA_old)
+		{
+			len_FA_old = len_FA;
+		}
+		printf("%i ", len_FA);
+		printf("%i \n", len_FA_old);
+	}
+	for(ii = 0; ii < 5; ii++)
+	{
+		for(jj = 0; jj < 2*max_build_steps; jj++)
+		{
+			printf("%f, ",  init_pos[ind3D(jj, 0, ii, 2*max_build_steps, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(jj, 1, ii, 2*max_build_steps, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(jj, 2, ii, 2*max_build_steps, 5, 2)]);
+			printf("%f, ",  init_pos[ind3D(jj, 3, ii, 2*max_build_steps, 5, 2)]);
+			printf("%f, \n",init_pos[ind3D(jj, 4, ii, 2*max_build_steps, 5, 2)]);
+		}
+		printf("\n");
+	}
+	/*
 	len_FA = new_point_pos((init_pos+ind3D(0,0,0, 2*max_build_steps, 5, 2)), tmp_sp, len_FA, FA1);
 	len_FA = new_point_pos((init_pos+ind3D(0,0,1, 2*max_build_steps, 5, 2)), tmp_sp, len_FA, FA1);
 
@@ -42,6 +70,6 @@ void create_init_state()
 		printf("%f, ",  init_pos[ind3D(ii, 2, 1, 2*max_build_steps, 5, 2)]);
 		printf("%f, ",  init_pos[ind3D(ii, 3, 1, 2*max_build_steps, 5, 2)]);
 		printf("%f, \n",init_pos[ind3D(ii, 4, 1, 2*max_build_steps, 5, 2)]);
-	}
+	}*/
 }
 
