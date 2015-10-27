f0463acf6277571b029ef94b52de0b664195b910
ntd14 <ntd14@uclive.ac.nz>
2015-10-27 16:13:10 +1300

    still tying to work out how to build the cml

---
+++ src/helpers/make_FA_chain.c
@@ -0,0 +1,17 @@
+/*
+ * make_FA_chain.c
+ *
+ *  Created on: 27/10/2015
+ *      Author: nick
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "get_ini_vars.h"
+#include "make_FA_chain.h"
+
+#include "cart_and_cyl.h"
+#include "make_arrays.h"
+#include "normal_dist_gen.h"
+
+
